86b0e2d6454c2d4dbadf763a131fcf171a03cf80
Richard Guan <guanyutian2013@gmail.com>
2020-02-29 22:18:27 -0500

    edit

--- edit_edit.php
+++ edit_edit.php
@@ -2,19 +2,14 @@
 require('db_connect.php');
 	
 // Assigning POST values to variables.
+$id = $GET['trans'];
 $date = $_POST['trans_date'];
 $transition = $_POST['transition'];
 $inflow = $_POST['in_flow'];
 $outflow = $_POST['out_flow'];
 
-$query = "INSERT INTO transaction_info(transaction_date, transaction_type, transaction_outflow, transaction_inflow) VALUES ('$date', '$transition', '$outflow', '$inflow')";
+$query = "INSERT INTO transaction_info(transaction_id, transaction_date, transaction_type, transaction_outflow, transaction_inflow) VALUES ($id, '$date', '$transition', '$outflow', '$inflow')";
 
-if (pg_query($db_connection, $query)){
-	echo "<script type='text/javascript'>alert('Changes Saved')</script>";
-	echo "<script> window.location.href = 'Main.html' </script>";
-} else{
-	echo "ERROR: Could not able to execute $sql. " . pg_error($db_connection);
-}
-
+pg_query($db_connection, $query);
 
 ?>
