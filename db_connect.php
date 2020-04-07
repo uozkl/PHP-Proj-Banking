5a33817729c7a7394b88b87d8305308908fd454a
Zekun Li <zli203@uottawa.ca>
2020-04-07 16:03:50 -0400

    update db url

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 /* Attempt to connect to database */
-$db_connection = pg_connect("host=192.168.0.120 dbname=web_proj user=user password=user");
+$db_connection = pg_connect("host=zklhome.ddns.net dbname=web_proj user=user password=user");
  
 // Check connection
 if($db_connection === false){
